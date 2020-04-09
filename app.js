f5c7bd12219af511cafb1ba159b8c6f787268842
Mike <ccarhuas.mike@gmail.com>
2020-04-09 17:09:32 -0500

    validacion de listado

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
